fdb23755333513d8153f83ed3f1303507c3aef9b
bryandsanti <124901691+bryandsanti@users.noreply.github.com>
2024-09-28 21:56:02 -0400

    Completed the 5 rounds of gaming

--- script.js
+++ script.js
@@ -1,31 +1,94 @@
 // Computer's Choice //
 
 
-function getComputerChoice(min , max){
-    return Math.floor(Math.random() * (max-min) + min);
+function getComputerChoice(){
+   let computerAnswer = Math.floor(Math.random() * 3);
+
+    
+    if (computerAnswer == 0){
+        return 'Rock';
+    } else if (computerAnswer == 1){
+        return 'Paper';
+    } else {
+        return 'Scissors'
+    }
 }
 
-let computerAnswer = getComputerChoice(1,4);
-
-console.log(computerAnswer);
-
-if (computerAnswer ==  1){
-    console.log('Rock');
-} else if (computerAnswer == 2){
-    console.log('Paper');
-} else {
-    console.log('Scissors');
-}
+const computerChoice = getComputerChoice();
+// console.log(computerChoice)
 
 // Human's Choice //
 
+function getHumanChoice(){
 let humanChoice = prompt('What is your choice?').toLowerCase();
  
-if ( (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') && humanChoice != null && humanChoice.length != 0 ){
-    console.log(humanChoice.charAt(0).toLocaleUpperCase() + humanChoice.slice(1));
+if ( (humanChoice=== 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') && humanChoice != null && humanChoice.length != 0 ){
+    console.log('You chose:' , humanChoice.charAt(0).toLocaleUpperCase() + humanChoice.slice(1));
 } else {
     alert('Please pick a valid choice!')
 }
-
+}
 // Comparing answers //
 
+let humanScore = 0;
+let computerScore = 0;
+
+console.log('Initial Score:');
+console.log('Human Score: ', humanScore);
+console.log('Computer Score: ', computerScore);
+
+function playRound(humanChoice, computerChoice){
+
+    if (humanChoice === 'Rock'){
+        if (computerChoice === 'Scissors'){
+            computerScore++;
+            console.log('Computer Wins!');
+        } else if (computerChoice === 'Paper'){
+            humanScore++;
+            console.log('You Win!');
+        } else {
+            console.log ("It's a tie!!");
+        }
+    } else if (humanChoice === 'Paper'){
+        if (computerChoice === 'Rock'){
+            humanScore++;
+            console.log('You win!');
+        } else if (computerChoice === 'Scissors'){
+            computerScore++;
+            console.log('Computer Wins!');
+        } else {
+            console.log("It's a tie!!");
+        }
+    } else {
+        if (computerChoice === 'Paper'){
+            humanScore++;
+            console.log('You Win!');
+        } else if (computerChoice === 'Rock'){
+            computerScore++;
+            console.log('Computer Wins!');
+        } else {
+            console.log("It's a tie!!");
+        }
+    }
+    console.log('Human Score: ', humanScore);
+    console.log('Computer Score: ', computerScore);
+}
+
+// Making the game 5 rounds //
+
+function playGame(){
+    for( let i = 0; i < 5; i++){
+        const humanChoice = getHumanChoice();
+        const computerChoice= getComputerChoice();
+        
+        playRound(humanChoice,computerChoice);
+    }
+
+    if(humanScore > computerScore){
+        console.log('You Won!!');
+    } else {
+        console.log('You lost!! Better luck next time!')
+    }
+}
+
+playGame();
